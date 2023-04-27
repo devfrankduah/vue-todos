7b6b812acb3d3da3f810f09150fed60f57fded42
Frank Duah <frankduah0@gmail.com>
2023-04-27 06:15:38 +0000

    --added header --added font, imgs and styles

---
+++ vue-todos/src/components/TodoHeader.vue
@@ -0,0 +1,24 @@
+<template>
+<header>
+    <nav class="container">
+        <div class="branding">
+            <img src="../assets/Vue_Logo_Black.png" alt="Vue Logo" />
+            <h1>Vue Todos</h1>
+        </div>
+        <ul class="nav-routers">
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+        </ul>
+    </nav>
+</header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
